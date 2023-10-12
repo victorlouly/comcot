da967b86beef0001af61b4b7ab5348dfd086cb40
victorlouly <91222566+victorlouly@users.noreply.github.com>
2023-10-12 20:12:49 -0300

    fefe

--- frontend/src/translate/languages/pt.js
+++ frontend/src/translate/languages/pt.js
@@ -311,7 +311,7 @@ const messages = {
           todolist: "Tarefas",
           quickMessages: "Respostas Rápidas",
           contacts: "Contatos",
-          queues: "Filas & Chatbot",
+          queues: "Departamentos & Chatbot",
           tags: "Tags",
           administration: "Administração",
           users: "Usuários",
@@ -548,7 +548,7 @@ const messages = {
         },
       },
       queueSelect: {
-        inputLabel: "Filas",
+        inputLabel: "Departamentos",
       },
       users: {
         title: "Usuários",
